22719f99cf45ccfd907402744d633100eae548f7
vothanhbinh <vo.binh224@gmail.com>
2020-05-06 14:35:12 +0700

    edit xíu xíu

--- Clinic/ViewModel/MainViewModel.cs
+++ Clinic/ViewModel/MainViewModel.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Effects;
 
 namespace Clinic.ViewModel
