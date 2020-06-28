a1f705cd0e5b986729a3aec5c9ae797d56d0329e
Ryuujin <longhairbees@gmail.com>
2020-06-28 13:09:30 +0700

    Thêm hàm tính tiền cho hóa đơn :)

--- DTO_Clinic/DTO_HoaDon.cs
+++ DTO_Clinic/DTO_HoaDon.cs
@@ -1,30 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DTO_Clinic
 {
-    public class DTO_HoaDon:BaseModel
+    public class DTO_HoaDon : BaseModel
     {
         public virtual DTO_CTPhieuKhamBenh CTPhieuKhamBenh { get; set; }
         public virtual DTO_PhieuKhamBenh PhieuKhamBenh { get; set; }
         public int Id { get; set; }
-        public float TienKham { get => _tienKham; set { _tienKham = value; OnPropertyChanged(); } }
-        public float TienThuoc { get => _tienThuoc; set { _tienThuoc = value; OnPropertyChanged(); } }
-        public float ThanhTien { get => _thanhTien; set { _thanhTien = value; OnPropertyChanged(); } }
+        public double TienKham { get => _tienKham; set { _tienKham = value; OnPropertyChanged(); } }
+        public double TienThuoc { get => _tienThuoc; set { _tienThuoc = value; OnPropertyChanged(); } }
+        public double ThanhTien { get => _thanhTien; set { _thanhTien = value; OnPropertyChanged(); } }
 
         public bool IsDeleted { get => _isDeleted; set { _isDeleted = value; OnPropertyChanged(); } }
 
-        private float _tienKham;
-        private float _tienThuoc;
-        private float _thanhTien;
+        private double _tienKham;
+        private double _tienThuoc;
+        private double _thanhTien;
         private bool _isDeleted;
 
-        public void TinhTien()
+        public double TinhTienThuoc(DTO_PhieuKhamBenh phieuKhamBenh)
         {
-            PhieuKhamBenh.DSCTPhieuKhamBenh.Count();
+            double tienThuoc = 0;
+            ICollection<DTO_CTPhieuKhamBenh> ListDSCT = phieuKhamBenh.DSCTPhieuKhamBenh;
+            foreach (DTO_CTPhieuKhamBenh item in ListDSCT)
+            {
+                tienThuoc += item.ThanhTien;
+            }
+            return tienThuoc;
+        }
+        public DTO_HoaDon()
+        {
+            IsDeleted = false;
+        }
+
+        public  DTO_HoaDon (DTO_PhieuKhamBenh phieuKhamBenh, double _tienKham)
+        {
+            TienThuoc = TinhTienThuoc(phieuKhamBenh);
+            TienKham = _tienKham;
+            ThanhTien = TienKham + TienThuoc;
+
+            IsDeleted = false;
         }
     }
 }
