8498efb5d3edd1e4a02762713cbaace7861936a5
Mercury <quocan.omg@gmail.com>
2020-07-01 22:41:27 +0700

    Format tiền + chỉ cho phép nhập số vào đơn giá và số lượng

--- GUI_Clinic/View/Windows/wdPhieuNhapThuoc.xaml.cs
+++ GUI_Clinic/View/Windows/wdPhieuNhapThuoc.xaml.cs
@@ -11,6 +11,7 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Policy;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -204,6 +205,31 @@ namespace GUI_Clinic.View.Windows
             Button b = sender as Button;
             DTO_Thuoc item = b.CommandParameter as DTO_Thuoc;
             List.Remove(item);
+        }
+
+        private void tbxDonGia_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static readonly Regex _regex = new Regex(@"([^0-9]+)|\s+", RegexOptions.Singleline); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+        private void tbxDonGia_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
         }
         //private void btnThemThuoc_Click(object sender, RoutedEventArgs e)
         //{
